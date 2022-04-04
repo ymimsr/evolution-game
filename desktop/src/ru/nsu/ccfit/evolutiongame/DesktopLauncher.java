ed489d56a31626db1f3fe1fbc5c62f8b6a3e08c4
ymim <43496855+ymimsr@users.noreply.github.com>
2022-04-05 02:54:55 +0700

    initial project structure

---
+++ desktop/src/ru/nsu/ccfit/evolutiongame/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.nsu.ccfit.evolutiongame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.nsu.ccfit.evolutiongame.EvolutionGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new EvolutionGame(), config);
+	}
+}
